e590ba057a221fe38d5d52c7efbc73e350e3c8d9
Ali Fakoor <ali.fakoor13@gmail.com>
2022-11-16 21:49:15 +0330

    fix bug in import task.schema

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -2,7 +2,7 @@ import { PoolModule } from '@modules/pool/pool.module';
 import { UserModule } from '@modules/user/user.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Task, TaskSchema } from 'schema/Task.schema';
+import { Task, TaskSchema } from 'schema/task.schema';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 
