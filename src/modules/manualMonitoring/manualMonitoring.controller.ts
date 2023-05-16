8e070ee725148a26a4cbe373bde37fd071b98f7e
Ali Fakoor <ali.fakoor13@gmail.com>
2023-05-16 17:51:20 +0430

    make optional manual monitoring apis

--- src/modules/manualMonitoring/manualMonitoring.controller.ts
+++ src/modules/manualMonitoring/manualMonitoring.controller.ts
@@ -19,30 +19,33 @@ import { ParamIdDto } from 'dto/paramId.dto';
 
 @Controller('manualMonitoring')
 @ApiTags('Manual Monitoring')
-@SetMetadata('isPublic', true)
 export class ManualMonitoringController {
   @Inject()
   private readonly manualMonitoringService: ManualMonitoringService;
 
   @Get(':id')
+  @SetMetadata('isPublic', true)
   @ApiOperation({ summary: 'اطلاعات اضافه' })
   async getManualMonitoring(@Param() param: ParamIdDto) {
     return await this.manualMonitoringService.getAll(param.id);
   }
 
   @Post('sampling')
+  @SetMetadata('isPublic', true)
   @ApiOperation({ summary: 'ثبت نمونه‌برداری' })
   async createSampling(@Body() body: CreateSamplingBodyDto): Promise<void> {
     await this.manualMonitoringService.createSampling(body);
   }
 
   @Post('feeding')
+  @SetMetadata('isPublic', true)
   @ApiOperation({ summary: 'ثبت غذادهی' })
   async createFeeding(@Body() body: CreateFeedingBodyDto): Promise<void> {
     await this.manualMonitoringService.createFeeding(body);
   }
 
   @Post('changingWater')
+  @SetMetadata('isPublic', true)
   @ApiOperation({ summary: 'ثبت تعویض آب' })
   async createWaterChanging(
     @Body() body: CreateChangingWaterBodyDto,
@@ -51,6 +54,7 @@ export class ManualMonitoringController {
   }
 
   @Post('transparency')
+  @SetMetadata('isPublic', true)
   @ApiOperation({ summary: 'ثبت شفافیت' })
   async createTransparency(
     @Body() body: CreateTransparencyBodyDto,
